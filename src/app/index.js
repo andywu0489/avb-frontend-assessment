3674f2d258651f4ee2eecac43ec42bb354d707e1
andywu0489 <andywu0489@gmail.com>
2021-05-08 01:02:27 -0400

    Create commentList component

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,15 @@
 import React from "react";
-
 import "app/App.css";
-import logo from "app/logo.svg";
 import Header from "components/Header";
 import CommentModal from "components/CommentModal";
+import CommentList from "components/CommentList";
 
 function App() {
   return (
     <>
       <Header />
-
       <CommentModal />
-
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
+      <CommentList />
     </>
   );
 }
