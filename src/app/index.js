352c166a89b8fb33977b7d603ef2b45f09201ebc
andywu0489 <andywu0489@gmail.com>
2021-05-10 09:56:15 -0400

    Create material ui theme

--- src/app/index.js
+++ src/app/index.js
@@ -3,15 +3,26 @@ import "app/App.css";
 import Header from "components/Header";
 import CommentModal from "components/CommentModal";
 import ListComponent from "components/List";
+import TopListModal from "components/TopListModal";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import pink from "@material-ui/core/colors/pink";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: pink,
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <CommentModal />
-      <ListComponent topThreeList={true} />
+      <TopListModal />
       <ListComponent />
-    </>
+    </ThemeProvider>
   );
 }
 
